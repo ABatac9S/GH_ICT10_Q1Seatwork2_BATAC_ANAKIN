83acf634cac9f664e8547b8fc6339cf7af1b2ab3
ABatac9S <batac.aq@obmontessori.edu.ph>
2025-09-14 20:32:43 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+#  Document
+
+x = 'Lilbits'
+y = 'Jack'
+year = 2025
+popular_item_price = 89.9
+popular_item_name = 'Lil pizza'
+has_delivery = False
+product_names = ['Lil burger', 'Lil taco', 'Lil siomai']
+business_hours = ['12:00', '8:00']
+menu_prices = ['']
+
+
+
+
+
+
+
+
+display(f'{x} Owner:{y}')
+display(f'Popular Choice:{popular_item_name}')
+display(f'Avalible:{has_delivery}')
+display(f'Food Options : {product_names[0]}, {product_names[1]}, {product_names[2]}')
